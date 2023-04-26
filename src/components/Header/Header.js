ddba78ee3fb25d279c21f3c4162cc34e072aaaf0
RAHUL <rahulmeetawa@gmail.com>
2023-04-26 21:23:36 +0530

    update

---
+++ src/components/Header/Header.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import "../../styles/style.css";
+
+function Header() {
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavbar] = useState(false);
+
+  function scrollHandler() {
+    if (window.scrollY >= 20) {
+      updateNavbar(true);
+    } else {
+      updateNavbar(false);
+    }
+  }
+
+  window.addEventListener("scroll", scrollHandler);
+
+  return (
+    <Navbar
+      expanded={expand}
+      fixed="top"
+      expand="md"
+      className={navColour ? "sticky" : "navbar"}
+    >
+      <Navbar.Brand className="logotext" as={Link} to="/">
+        <div className="logo"></div>
+      </Navbar.Brand>
+
+      <Navbar.Toggle
+        className="navbar-toggler"
+        aria-controls="responsive-navbar-nav"
+        onClick={() => {
+          updateExpanded(expand ? false : "expanded");
+        }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </Navbar.Toggle>
+      <Navbar.Collapse id="responsive-navbar-nav" className="responsive-navbar">
+        <Nav className="ms-auto" defaultActiveKey="#home">
+          <Nav.Item>
+            <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+              {" "}
+              Home{" "}
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              as={Link}
+              to="/about"
+              onClick={() => updateExpanded(false)}
+            >
+              About
+            </Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link
+              as={Link}
+              to="/project"
+              onClick={() => updateExpanded(false)}
+            >
+              Projects
+            </Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link
+              as={Link}
+              to="/contact"
+              onClick={() => updateExpanded(false)}
+            >
+              Contact
+            </Nav.Link>
+          </Nav.Item>
+
+          <Button
+            onClick={() => {
+              window.open(
+                "https://drive.google.com/file/d/1zbrcyEPX5vpSL0JVXJb7x8CUZu089nYK/view?usp=sharing"
+              );
+            }}
+            className="resumebtn"
+          >
+            <span>Resume</span>
+          </Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
-/* import Header from '../components/Header/Header'
-import Homepage from '../components/Homepage/Homepage' */
+import Header from "../components/Header/Header";
+/* import Homepage from '../components/Homepage/Homepage' */
 
 function Home() {
   return (
     <div>
-      {/*  <Header />
-      <Homepage /> */}
+      {
+        <Header />
+        /*<Homepage /> */
+      }
     </div>
   );
 }
