5182d70027f72bf33c5affa15042c54879839c47
RAHUL <rahulmeetawa@gmail.com>
2023-04-30 22:19:52 +0530

    update

--- src/components/Aboutpage/Aboutpage.js
+++ src/components/Aboutpage/Aboutpage.js
@@ -1,43 +1,49 @@
-import React from 'react'
-import '../../pages/style.css';
-import { Container, Row, Col } from 'react-bootstrap'
-
+import React from "react";
+import "../../pages/style.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Aboutpage() {
-    return (
-        <div className='aboutpagebackground'>
-            <Container>
-                <Row className='textbackground'>
-                    <Col md={7} >
-                        <h3 className='aboutmetext'>About <span>Me</span></h3>
-                        <p className='aboutdetails'>I'm a self-taught Front End developer from India. I build websites with a focus on providing the experience for everyone using them and responsiveness. Curious to learn more about developing scalable distribution systems, love problem solving, and care about writing and maintainable code. A responsive design makes your website accessible to all users, regardless of their devices.
-                        </p>
-                        <ul className='skilllist'>
-                            <Row>
-                                <h3>Skills</h3>
-                                <Col md={7}>
-                                    <li>HTML5/CSS3</li>
-                                    <li>JavaScript</li>
-                                    <li>jQuery</li>
-                                    <li>Bootsrap 5</li>
-                                </Col>
-                                <Col md={5}>
-                                    <li>React Js</li>
-                                    <li>Redux Js</li>
-                                    <li>React-Bootsrap</li>
-                                    <li>Material-ui</li>
-                                    <li>Git/Github</li>
-                                </Col>
-                            </Row>
-                        </ul>
-                    </Col>
-                    <Col md={5}>
-                        <div className="webimage"></div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
+  return (
+    <div className="aboutpagebackground">
+      <Container>
+        <Row className="textbackground">
+          <Col md={7}>
+            <h3 className="aboutmetext">
+              About <span>Me</span>
+            </h3>
+            <p className="aboutdetails">
+              I am a Frontend Developer.I like dabbling in various parts of
+              frontend development,Strong engineering professional with a
+              Bachelor's Degree focused in Computer Science and Technology from
+              B.K. Birla Institute of Engineering & Technology,pilani and like
+              to learn about new technologies A responsive design makes your
+              website accessible to all users, regardless of their devices.
+            </p>
+            <ul className="skilllist">
+              <Row>
+                <h3>Skills</h3>
+                <Col md={7}>
+                  <li>C/C++/C#</li>
+                  <li>OOPs</li>
+                  <li>DSA</li>
+                </Col>
+                <Col md={5}>
+                  <li>HTML5/CSS3</li>
+                  <li>javaScript/TypeScript</li>
+                  <li>React Js</li>
+                  <li>Node Js</li>
+                  <li>Git/Github</li>
+                </Col>
+              </Row>
+            </ul>
+          </Col>
+          <Col md={5}>
+            <div className="webimage"></div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default Aboutpage
+export default Aboutpage;
--- src/components/Contactpage/Contactpage.js
+++ src/components/Contactpage/Contactpage.js
@@ -1,27 +1,36 @@
-import React from 'react'
-import '../../pages/style.css';
-import { Container } from 'react-bootstrap'
+import React from "react";
+import "../../pages/style.css";
+import { Container } from "react-bootstrap";
 
 function Contactpage() {
   return (
-    <div className='contactbackground'>
+    <div className="contactbackground">
       <Container>
-        <h2 className='contacthead'>Get In Touch</h2>
-        <p className='contactpara'> I’m currently searching for opportunities for a front-end developer role. <br /> If there is any vacancy my inbox is always open. Whether <br /> you have any further questions or just want to say hi, <br /> I’ll try my best to get back to you!</p>
+        <h2 className="contacthead">Get In Touch</h2>
+        <p className="contactpara">
+          {" "}
+          I’m currently searching for opportunities for a front-end developer
+          role. <br /> If there is any vacancy my inbox is always open. Whether{" "}
+          <br /> you have any further questions or just want to say hi, <br />{" "}
+          I’ll try my best to get back to you!
+        </p>
         <button
-          className='contactbtn'
+          className="contactbtn"
           onClick={() => {
-            window.open("https://wa.me/+918156811884");
+            window.open("https://wa.me/+916350691734");
           }}
-        >Say Hello</button>
+        >
+          Say Hello
+        </button>
         <span></span>
-        <hr className='line' />
-        <p className='copyright'>© Copyright 2023.
-        <br /> 
-        Designed & Built by <span>rahulvijay</span></p>
+        <hr className="line" />
+        <p className="copyright">
+          © Copyright 2023.
+          <br />{" "}
+        </p>
       </Container>
     </div>
-  )
+  );
 }
 
-export default Contactpage
+export default Contactpage;
